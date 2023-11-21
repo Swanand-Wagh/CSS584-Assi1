71f2d909ed8a9618dd814188aca62888b461ae68
Swanand-Wagh <swanandwagh7@gmail.com>
2023-11-21 00:19:11 -0800

    misc

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -4,10 +4,8 @@ export const HeaderBar = () => {
   return (
     <header className="topHeader">
       <div className="topHeader__contentWrap">
-        <p className="topHeader__contentWrap__appTitle">
-          CSS 584 - <span> Assignment </span>
-        </p>
-        Swanand Wagh - 2330804
+        <p className="topHeader__contentWrap__appTitle">Image Retrieval System</p>
+        Swanand Wagh
       </div>
     </header>
   );
